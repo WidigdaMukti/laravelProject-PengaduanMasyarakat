82b3ae407d4895f2e8cccb926cfb75f55320eee6
WidigdaMukti <117274670+WidigdaMukti@users.noreply.github.com>
2023-07-21 19:08:34 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\Petugas::create([
+            'nama_petugas' => 'Adminstrator',
+            'username' => 'admin',
+            'password' => Hash::make('password'),
+            'telp' => '08990671253',
+            'level' => 'admin',
+        ]);
+    }
+}
